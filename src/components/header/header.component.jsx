479c05e66f283cbb083024801419aa80efe2ba6c
Antonio <toni.cm@gmail.com>
2020-04-26 20:22:58 -0400

    added addItem And removeItem functionality to checkout item component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,6 +14,7 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
 const Header = ({ currentUser, hidden }) => (
+    
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
